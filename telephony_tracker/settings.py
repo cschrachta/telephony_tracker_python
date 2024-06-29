095b4f57053c4a21598f7bba33cab3d63b79f581
Jeff Stock <jantonstock@gmail.com>
2024-06-29 13:54:26 -0700

    moved .env reference; set STATIC_ROOT

--- telephony_tracker/settings.py
+++ telephony_tracker/settings.py
@@ -19,7 +19,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, False))
-env.read_env(env_file='telephony/.env')
+env.read_env(env_file='telephony_tracker/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static_main/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_main/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'telephony/static'),)
 
 # Default primary key field type
